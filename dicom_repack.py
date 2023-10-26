55dfc58da522297e5980bf45643056b3781ef0b8
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-10-26 15:05:34 -0400

    update memory to 4G

--- dicom_repack.py
+++ dicom_repack.py
@@ -7,7 +7,7 @@ import numpy as np
 from chris_plugin import chris_plugin, PathMapper
 import pydicom as dicom
 import os
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 
 DISPLAY_TITLE = r"""
        _           _ _                                                 _    
@@ -40,7 +40,7 @@ parser.add_argument('-V', '--version', action='version',
     parser=parser,
     title='A DICOM repack plugin',
     category='',                 # ref. https://chrisstore.co/plugins
-    min_memory_limit='2Gi',    # supported units: Mi, Gi
+    min_memory_limit='4Gi',    # supported units: Mi, Gi
     min_cpu_limit='20000m',       # millicores, e.g. "1000m" = 1 CPU core
     min_gpu_limit=0              # set min_gpu_limit=1 to enable GPU
 )
