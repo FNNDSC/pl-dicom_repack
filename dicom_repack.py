d56d4bd8d3496a663568bf327b7dedf491660ccb
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-04-23 16:15:46 -0400

    version bump

--- dicom_repack.py
+++ dicom_repack.py
@@ -8,7 +8,7 @@ from chris_plugin import chris_plugin, PathMapper
 import pydicom as dicom
 import os
 from pflog import pflog
-__version__ = '1.1.6'
+__version__ = '1.1.8'
 
 DISPLAY_TITLE = r"""
        _           _ _                                                 _    
